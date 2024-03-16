549c6f2b533748e60f5e794c91fc24e85f33f58c
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-03-16 13:43:27 +0200

    add function swiper-slider; lib body-scroll-lock

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from 'swiper/modules';
 
+import "swiper/css";
+import "swiper/css/pagination";
 import css from "./Hero.module.css";
 
 function Hero() {
@@ -12,48 +16,65 @@ function Hero() {
 
         <div className={`${css.swiper} ${css.heroImg}`}>
           <ul className={`${css.swiperWrapper} ${css.heroImgSlider}`}>
-            <li className={css.swiperSlide}>
-              <img
-                srcSet={`
+            <Swiper
+              slidesPerView={"auto"}
+              centeredSlides={true}
+              spaceBetween={30}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Pagination]}
+              className={css.mySwiper}
+            >
+              <SwiperSlide>
+                <li className={css.swiperSlide}>
+                  <img
+                    srcSet={`
             ${process.env.PUBLIC_URL}/images/mob/hero-slide/hero-mob1@2_375.png 750w,
             ${process.env.PUBLIC_URL}/images/mob/hero-slide/hero-mob1_375.png 375w
             `}
-                sizes="(min-width: 375px) 375px, 100vh"
-                src={
-                  process.env.PUBLIC_URL +
-                  "/images/mob/hero-slide/hero-mob1_375.png"
-                }
-                alt="gray sofa and pouf"
-              />
-            </li>
-            <li className={css.swiperSlide}>
-            <img
-                srcSet={`
+                    sizes="(min-width: 375px) 375px, 100vh"
+                    src={
+                      process.env.PUBLIC_URL +
+                      "/images/mob/hero-slide/hero-mob1_375.png"
+                    }
+                    alt="gray sofa and pouf"
+                  />
+                </li>
+              </SwiperSlide>
+              <SwiperSlide>
+                <li className={css.swiperSlide}>
+                  <img
+                    srcSet={`
             ${process.env.PUBLIC_URL}/images/mob/hero-slide/hero-mob2@2_375.png 2x,
             ${process.env.PUBLIC_URL}/images/mob/hero-slide/hero-mob2_375.png 1x
             `}
-                sizes="(min-width: 375px) 375px, 100vh"
-                src={
-                  process.env.PUBLIC_URL +
-                  "./images/mob/hero-slide/hero-mob2_375.png"
-                }
-                alt="gray table and white pouf"
-              />
-            </li>
-            <li className={css.swiperSlide}>
-            <img
-                srcSet={`
+                    sizes="(min-width: 375px) 375px, 100vh"
+                    src={
+                      process.env.PUBLIC_URL +
+                      "./images/mob/hero-slide/hero-mob2_375.png"
+                    }
+                    alt="gray table and white pouf"
+                  />
+                </li>
+              </SwiperSlide>
+              <SwiperSlide>
+                <li className={css.swiperSlide}>
+                  <img
+                    srcSet={`
             ${process.env.PUBLIC_URL}/images/mob/hero-slide/hero-mob3@2_375.png 750w,
             ${process.env.PUBLIC_URL}/images/mob/hero-slide/hero-mob3_375.png 375w
             `}
-                sizes="(min-width: 375px) 375px, 100vh"
-                src={
-                  process.env.PUBLIC_URL +
-                  "./images/mob/hero-slide/hero-mob3_375.png"
-                }
-                alt="gray table and green pouf"
-              />
-            </li>
+                    sizes="(min-width: 375px) 375px, 100vh"
+                    src={
+                      process.env.PUBLIC_URL +
+                      "./images/mob/hero-slide/hero-mob3_375.png"
+                    }
+                    alt="gray table and green pouf"
+                  />
+                </li>
+              </SwiperSlide>
+            </Swiper>
           </ul>
           <div className={css.swiperPagination}></div>
         </div>
