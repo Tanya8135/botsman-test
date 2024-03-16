f327c2ef639529c1a72c5f4671534785309a89f8
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-03-16 17:17:00 +0200

    started burger-menu

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -3,15 +3,109 @@ import { IoCloseOutline } from "react-icons/io5";
 
 import css from "./BurgerMenu.module.css";
 
-function BurgerMenu() {
+function BurgerMenu({ isMenuOpen, toggleMenu }) {
+  const closeMenu = () => {
+    toggleMenu();
+  };
+
   return (
-    <div className={`${css.menuContainer} ${css.jsMenuContainer}`}>
+    <div
+      className={`${css.menuContainer} ${
+        isMenuOpen ? css.jsMenuContainer : ""
+      }`}
+    >
       {/* btn-close */}
-      <button
-        className={`${css.menuToggle} ${css.menuClose} ${css.jsCloseMenu}`}
-      >
-        <IoCloseOutline className={css.menuCloseIcon} />
-      </button>
+      {isMenuOpen && (
+        <div className={css.burgerMenu}>
+          <button
+            className={`${css.menuToggle} ${css.menuClose} ${css.jsCloseMenu}`}
+            onClick={closeMenu}
+          >
+            <IoCloseOutline className={css.menuCloseIcon} />
+          </button>
+
+          <div className={css.burgerMenu__box}>
+            <ul className={css.burgerMenu__list}>
+              <li className={css.burgerMenu__item}>
+                <a href="/catalog.html" className={css.burgerMenu__link}>
+                  Каталог
+                </a>
+              </li>
+              <li className={css.burgerMenu__item}>
+                <a href="#individ-furniture" className={css.burgerMenu__link}>
+                  Индивидуальная мебель
+                </a>
+              </li>
+              <li className={css.burgerMenu__item}>
+                <a href="#our-history" className={css.burgerMenu__link}>
+                  Наша история
+                </a>
+              </li>
+              <li className={css.burgerMenu__item}>
+                <a href="#public" className={css.burgerMenu__link}>
+                  Публикации
+                </a>
+              </li>
+              <li className={css.burgerMenu__item}>
+                <a href="#info" className={css.burgerMenu__link}>
+                  Информация
+                </a>
+              </li>
+              <li className={css.burgerMenu__item}>
+                <a href="#contact" className={css.burgerMenu__link}>
+                  Контакты
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className={css.bmS}>
+            <ul className={css.bmS__list}>
+              <li className={css.bmS__item}>
+                <a
+                  href="/"
+                  target="_blank"
+                  rel="noopener nofollow noreferrer"
+                  className={css.bmS__link}
+                >
+                  <svg width="42" height="42">
+                    <use href="./images/icons.svg#icon-instagram"></use>
+                  </svg>
+                </a>
+              </li>
+              <li className={css.bmS__item}>
+                <a
+                  href="/"
+                  target="_blank"
+                  rel="noopener nofollow noreferrer"
+                  className={css.bmS__link}
+                >
+                  <svg width="42" height="42">
+                    <use href="./images/icons.svg#icon-facebook"></use>
+                  </svg>
+                </a>
+              </li>
+              <li className={css.bmS__item}>
+                <a
+                  href="/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={css.bmS__link}
+                >
+                  <svg width="42" height="42">
+                    <use href="./images/icons.svg#icon-telegram"></use>
+                  </svg>
+                </a>
+              </li>
+            </ul>
+            <div className="bm-s__tel">
+              <a href="tel:+380996384537" className={css.bmS__link}>
+                +38 (099)-638-45-37
+              </a>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,43 +1,49 @@
-import React from 'react';
-import logo from '../../img/logoBotsmanMin.svg';
-import { SlPhone } from 'react-icons/sl';
-import { FiMenu } from 'react-icons/fi';
-import css from './Header.module.css';
+import React, { useState } from "react";
+import logo from "../../img/logoBotsmanMin.svg";
+import { SlPhone } from "react-icons/sl";
+import { FiMenu } from "react-icons/fi";
+import css from "./Header.module.css";
 
-import BurgerMenu from 'components/BurgerMenu/BurgerMenu';
+import BurgerMenu from "components/BurgerMenu/BurgerMenu";
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={`${css.header} ${css.pageHeader}`}>
       <div className={`${css.headerContainer} ${css.container}`}>
         <div className={css.headerMob}>
           <div className={css.logo}>
-            <a href='./index.html' className={css.logoS}>
-              <img src={logo} alt='logo' className={css.logoS} />
+            <a href="./index.html" className={css.logoS}>
+              <img src={logo} alt="logo" className={css.logoS} />
             </a>
           </div>
 
           <div className={css.headerNavMob}>
             <div className={css.iconPhoneBox}>
-              <a href='tel:+380964875185' className={css.iconPhone1}>
+              <a href="tel:+380964875185" className={css.iconPhone1}>
                 <SlPhone className={css.iconPhone} />
               </a>
             </div>
 
             <div className={css.burgerMenuBox}>
               <button
-                type='button'
+                type="button"
                 className={`${css.btnBurgerMenu}`}
-                aria-expanded='false'
-                aria-controls='mobile-menu'
+                aria-expanded={isMenuOpen}
+                aria-controls="mobile-menu"
+                onClick={toggleMenu}
               >
-                <FiMenu className={css.burgerMenuIcon}/>
+                <FiMenu className={css.burgerMenuIcon} />
               </button>
             </div>
           </div>
         </div>
       </div>
-      <BurgerMenu/>
+      <BurgerMenu isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
     </header>
   );
 }
