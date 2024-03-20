78c262afa474f8bb611da6f39de46bb10993e43e
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-03-20 20:56:49 +0200

    broken down into components: FormBlanc

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,6 +2,7 @@ import React from "react";
 // import { IoCheckmark } from "react-icons/io5";
 
 import css from "./Form.module.css";
+import FormBlank from "./FormBlank/FormBlank";
 
 function Form() {
   return (
@@ -17,67 +18,7 @@ function Form() {
             <div className={css.formSubtitleBox}>
               <span className={css.formSubtitle}>остались вопросы?</span>
             </div>
-
-            <div className={css.formBlock}>
-              <form className={css.form}>
-                <label className={css.dataField}>
-                  <input
-                    type="text"
-                    name="name"
-                    className={`${css.dataField__input} ${css.inputHasValue}`}
-                    placeholder="Ваше имя*"
-                  />
-                </label>
-                <label className={css.dataField}>
-                  <input
-                    type="tel"
-                    name="tel"
-                    className={`${css.dataField__input} ${css.inputHasValue}`}
-                    placeholder="Ваш номер телефона*"
-                  />
-                </label>
-                <label className={css.dataField}>
-                  <input
-                    type="text"
-                    name="country"
-                    className={`${css.dataField__input} ${css.inputHasValue}`}
-                    placeholder="Страна"
-                  />
-                </label>
-                <label className={css.dataField}>
-                  <input
-                    type="text"
-                    name="comment"
-                    className={`${css.dataField__input} ${css.inputHasValue}`}
-                    placeholder="Комментарий"
-                  />
-                </label>
-
-                <div className={css.policyBox}>
-                  <label className={css.policy}>
-                    <div className={css.checkboxBox}>
-                      <input
-                        className={css.policy__checkbox}
-                        type="checkbox"
-                        name="policy"
-                        value="css"
-                      />
-                      <span className={css.policy__icon}></span>
-
-                      <span className={css.policy__text}>
-                        {/* <IoCheckmark className={css.checkmark} /> */}
-                        Подтверждая заказ, я принимаю условия пользовательского
-                        соглашения
-                      </span>
-                    </div>
-                  </label>
-                </div>
-
-                <button type="button" className="btn">
-                  Запросить звонок
-                </button>
-              </form>
-            </div>
+            <FormBlank />
           </div>
         </div>
       </section>
