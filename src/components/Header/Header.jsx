d3a377f9f66b84cc564ba63ced81c3b3e360f665
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-03-20 16:05:36 +0200

    broken component Header on components: HeaderLogo, HeaderNav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,49 +1,18 @@
-import React, { useState } from "react";
-import logo from "../../img/logoBotsmanMin.svg";
-import { SlPhone } from "react-icons/sl";
-import { FiMenu } from "react-icons/fi";
-import css from "./Header.module.css";
+import React from "react";
 
-import BurgerMenu from "components/BurgerMenu/BurgerMenu";
+import css from "./Header.module.css";
+import HeaderNav from "components/Header/HeaderNav/HeaderNav";
+import HeaderLogo from "./HeaderLogo/HeaderLogo";
 
 function Header() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
     <header className={`${css.header} ${css.pageHeader}`}>
       <div className={`${css.headerContainer} ${css.container}`}>
         <div className={css.headerMob}>
-          <div className={css.logo}>
-            <a href="./index.html" className={css.logoS}>
-              <img src={logo} alt="logo" className={css.logoS} />
-            </a>
-          </div>
-
-          <div className={css.headerNavMob}>
-            <div className={css.iconPhoneBox}>
-              <a href="tel:+380964875185" className={css.iconPhone1}>
-                <SlPhone className={css.iconPhone} />
-              </a>
-            </div>
-
-            <div className={css.burgerMenuBox}>
-              <button
-                type="button"
-                className={`${css.btnBurgerMenu}`}
-                aria-expanded={isMenuOpen}
-                aria-controls="mobile-menu"
-                onClick={toggleMenu}
-              >
-                <FiMenu className={css.burgerMenuIcon} />
-              </button>
-            </div>
-          </div>
+          <HeaderLogo />
+          <HeaderNav />
         </div>
       </div>
-      <BurgerMenu isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
     </header>
   );
 }
