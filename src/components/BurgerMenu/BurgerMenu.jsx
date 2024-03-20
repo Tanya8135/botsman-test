b1e3186d45478e625a8b16573700257cab043a8e
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-03-20 20:13:18 +0200

    broken down into components

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import { IoCloseOutline } from "react-icons/io5";
-import { BsInstagram } from "react-icons/bs";
-import { RiFacebookFill } from "react-icons/ri";
-import { FaTelegramPlane } from "react-icons/fa";
+// import { IoCloseOutline } from "react-icons/io5";
 
 import css from "./BurgerMenu.module.css";
+import BtnClose from "./BtnClose/BtnClose";
+import BurgerMenuBox from "./BurgerMenuBox/BurgerMenuBox";
+import BurgerMenuSocial from "./BurgerMenuSocial/BurgerMenuSocial";
 
 function BurgerMenu({ isMenuOpen, toggleMenu }) {
-  const closeMenu = () => {
-    toggleMenu();
-  };
-
   return (
     <div
       className={`${css.menuContainer} ${
@@ -20,87 +16,9 @@ function BurgerMenu({ isMenuOpen, toggleMenu }) {
       {/* btn-close */}
       {isMenuOpen && (
         <div className={css.burgerMenu}>
-          <button
-            className={`${css.menuToggle} ${css.menuClose} ${css.jsCloseMenu}`}
-            onClick={closeMenu}
-          >
-            <IoCloseOutline className={css.menuCloseIcon} />
-          </button>
-
-          <div className={css.burgerMenu__box}>
-            <ul className={css.burgerMenu__list}>
-              <li className={css.burgerMenu__item}>
-                <a href="/catalog.html" className={css.burgerMenu__link}>
-                  Каталог
-                </a>
-              </li>
-              <li className={css.burgerMenu__item}>
-                <a href="#individ-furniture" className={css.burgerMenu__link}>
-                  Индивидуальная мебель
-                </a>
-              </li>
-              <li className={css.burgerMenu__item}>
-                <a href="#our-history" className={css.burgerMenu__link}>
-                  Наша история
-                </a>
-              </li>
-              <li className={css.burgerMenu__item}>
-                <a href="#public" className={css.burgerMenu__link}>
-                  Публикации
-                </a>
-              </li>
-              <li className={css.burgerMenu__item}>
-                <a href="#info" className={css.burgerMenu__link}>
-                  Информация
-                </a>
-              </li>
-              <li className={css.burgerMenu__item}>
-                <a href="#contact" className={css.burgerMenu__link}>
-                  Контакты
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          <div className={css.bmS}>
-            <ul className={css.bmS__list}>
-              <li className={css.bmS__item}>
-                <a
-                  href="/"
-                  target="_blank"
-                  rel="noopener nofollow noreferrer"
-                  className={css.bmS__link}
-                >
-                  <BsInstagram className={css.socialIconBM} />
-                </a>
-              </li>
-              <li className={css.bmS__item}>
-                <a
-                  href="/"
-                  target="_blank"
-                  rel="noopener nofollow noreferrer"
-                  className={css.bmS__link}
-                >
-                  <RiFacebookFill className={css.socialIconBM} />
-                </a>
-              </li>
-              <li className={css.bmS__item}>
-                <a
-                  href="/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={css.bmS__link}
-                >
-                  <FaTelegramPlane className={css.socialIconBM} />
-                </a>
-              </li>
-            </ul>
-            <div className={css.bmS__tel}>
-              <a href="tel:+380996384537" className={css.bmS__link}>
-                +38 (099)-638-45-37
-              </a>
-            </div>
-          </div>
+          <BtnClose toggleMenu={toggleMenu} />
+          <BurgerMenuBox />
+          <BurgerMenuSocial />
         </div>
       )}
     </div>
