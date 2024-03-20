4cb63239db902ebd3e8fd0bd0c60f2551c097269
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-03-20 09:59:33 +0200

    add last-public component (section)

--- src/components/LastPublic/LastPublic.jsx
+++ src/components/LastPublic/LastPublic.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { BsArrowRight } from "react-icons/bs";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+import "swiper/css";
+import "swiper/css/scrollbar";
 import css from "./LastPublic.module.css";
+
+import { Scrollbar } from "swiper/modules";
 
 function LastPublic() {
   return (
@@ -16,9 +22,7 @@ function LastPublic() {
           <a href="./catalog.html" className={css.lpCatalog}>
             Перейти до каталогу
             <div className={css.lpArrow}>
-              <svg width="24" height="24">
-                <use href="./images/icons.svg#icon-arrow"></use>
-              </svg>
+              <BsArrowRight className={css.arrowNext} />
             </div>
           </a>
         </div>
@@ -26,146 +30,166 @@ function LastPublic() {
         <article className={css.cardProd}>
           <div className={`${css.swiperContainer} ${css.lpImg}`}>
             <ul className={`${css.swiperWrapper} ${css.lpImgList}`}>
-              <li className={`${css.swiperSlide} ${css.lpItem}`}>
-                <article className={`${css.lpCard} ${css.overlayCard}`}>
-                  <div className={css.lpImgBox}>
-                    <img
-                      srcSet={`
+              <Swiper
+                scrollbar={{
+                  hide: false,
+                }}
+                modules={[Scrollbar]}
+                className={css.mySwiper}
+              >
+                <SwiperSlide>
+                  <li className={`${css.swiperSlide} ${css.lpItem}`}>
+                    <article className={`${css.lpCard} ${css.overlayCard}`}>
+                      <div className={css.lpImgBox}>
+                        <img
+                          srcSet={`
                 ${process.env.PUBLIC_URL}/images/mob/public-slide/pink_pouf@2_mob_270.png 540w,
                 ${process.env.PUBLIC_URL}/images/mob/public-slide/pink_pouf_mob_270.png   270w
                 `}
-                      sizes="(min-width: 375px) 270px, 100vw"
-                      src={
-                        process.env.PUBLIC_URL +
-                        "/images/mob/public-slide/pink_pouf_mob_270.png"
-                      }
-                      alt="Pink puof"
-                    />
-                    <p className={css.overlayText}>
-                      Краткая информация. Цена <a href="/">link</a>
-                    </p>
-                  </div>
+                          sizes="(min-width: 375px) 270px, 100vw"
+                          src={
+                            process.env.PUBLIC_URL +
+                            "/images/mob/public-slide/pink_pouf_mob_270.png"
+                          }
+                          alt="Pink puof"
+                        />
+                        <p className={css.overlayText}>
+                          Краткая информация. Цена <a href="/">link</a>
+                        </p>
+                      </div>
 
-                  <p className={css.lpName}>pink pouf</p>
-                </article>
-              </li>
-              <li className={`${css.swiperSlide} ${css.lpItem}`}>
-                <article className={css.lpCard}>
-                  <div className={`${css.lpImgBox} ${css.overlayCard}`}>
-                    <img
-                      srcSet={`
+                      <p className={css.lpName}>pink pouf</p>
+                    </article>
+                  </li>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <li className={`${css.swiperSlide} ${css.lpItem}`}>
+                    <article className={css.lpCard}>
+                      <div className={`${css.lpImgBox} ${css.overlayCard}`}>
+                        <img
+                          srcSet={`
                       ${process.env.PUBLIC_URL}/images/mob/public-slide/black_pouf@2_mob_270.png 540w,
                       ${process.env.PUBLIC_URL}/images/mob/public-slide/black_pouf@2_mob_270.png 540w
                       `}
-                      sizes="(min-width: 375px) 270px, 100vw"
-                      src={
-                        process.env.PUBLIC_URL +
-                        "/images/mob/public-slide/black_pouf_mob_270.png"
-                      }
-                      alt="Black puof"
-                    />
+                          sizes="(min-width: 375px) 270px, 100vw"
+                          src={
+                            process.env.PUBLIC_URL +
+                            "/images/mob/public-slide/black_pouf_mob_270.png"
+                          }
+                          alt="Black puof"
+                        />
 
-                    <p className={css.overlayText}>
-                      Краткая информация. Цена <a href="/">link</a>
-                    </p>
-                  </div>
+                        <p className={css.overlayText}>
+                          Краткая информация. Цена <a href="/">link</a>
+                        </p>
+                      </div>
 
-                  <p className={css.lpName}>Black puof</p>
-                </article>
-              </li>
-              <li className={`${css.swiperSlide} ${css.lpItem}`}>
-                <article className={css.lpCard}>
-                  <div className={`${css.lpImgBox} ${css.overlayCard}`}>
-                    <img
-                      srcSet={`
+                      <p className={css.lpName}>Black puof</p>
+                    </article>
+                  </li>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <li className={`${css.swiperSlide} ${css.lpItem}`}>
+                    <article className={css.lpCard}>
+                      <div className={`${css.lpImgBox} ${css.overlayCard}`}>
+                        <img
+                          srcSet={`
                     ${process.env.PUBLIC_URL}/images/mob/public-slide/blue_pouf@2_mob_270.png 540w,
                     ${process.env.PUBLIC_URL}/images/mob/public-slide/blue_pouf_mob_270.png 270w
                     `}
-                      sizes="(min-width: 375px) 270px, 100vw"
-                      src={
-                        process.env.PUBLIC_URL +
-                        "/images/mob/public-slide/blue_pouf_mob_270.png"
-                      }
-                      alt="Blue puof"
-                    />
+                          sizes="(min-width: 375px) 270px, 100vw"
+                          src={
+                            process.env.PUBLIC_URL +
+                            "/images/mob/public-slide/blue_pouf_mob_270.png"
+                          }
+                          alt="Blue puof"
+                        />
 
-                    <p className={css.overlayText}>
-                      Краткая информация. Цена <a href="/">link</a>
-                    </p>
-                  </div>
-                  <p className={css.lpName}>Blue puof</p>
-                </article>
-              </li>
-              <li classNamclassName={`${css.swiperSlide} ${css.lpItem}`}>
-                <article className={css.lpCard}>
-                  <div className={`${css.lpImgBox} ${css.overlayCard}`}>
-                    <img
-                      srcSet={`
+                        <p className={css.overlayText}>
+                          Краткая информация. Цена <a href="/">link</a>
+                        </p>
+                      </div>
+                      <p className={css.lpName}>Blue puof</p>
+                    </article>
+                  </li>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <li className={`${css.swiperSlide} ${css.lpItem}`}>
+                    <article className={css.lpCard}>
+                      <div className={`${css.lpImgBox} ${css.overlayCard}`}>
+                        <img
+                          srcSet={`
                     ${process.env.PUBLIC_URL}/images/mob/public-slide/gray_pouf@2_mob_270.png 540w,
                     ${process.env.PUBLIC_URL}/images/mob/public-slide/gray_pouf_mob_270.png 270w
                     `}
-                      sizes="(min-width: 375px) 270px, 100vw"
-                      src={
-                        process.env.PUBLIC_URL +
-                        "/images/mob/public-slide/gray_pouf_mob_270.png"
-                      }
-                      alt="Gray puof"
-                    />
+                          sizes="(min-width: 375px) 270px, 100vw"
+                          src={
+                            process.env.PUBLIC_URL +
+                            "/images/mob/public-slide/gray_pouf_mob_270.png"
+                          }
+                          alt="Gray puof"
+                        />
 
-                    <p className={css.overlayText}>
-                      Краткая информация. Цена <a href="/">link</a>
-                    </p>
-                  </div>
-                  <p className={css.lpName}>Gray puof</p>
-                </article>
-              </li>
-              <li classNamclassName={`${css.swiperSlide} ${css.lpItem}`}>
-                <article className={css.lpCard}>
-                  <div className={`${css.lpImgBox} ${css.overlayCard}`}>
-                    <img
-                      srcSet={`
+                        <p className={css.overlayText}>
+                          Краткая информация. Цена <a href="/">link</a>
+                        </p>
+                      </div>
+                      <p className={css.lpName}>Gray puof</p>
+                    </article>
+                  </li>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <li className={`${css.swiperSlide} ${css.lpItem}`}>
+                    <article className={css.lpCard}>
+                      <div className={`${css.lpImgBox} ${css.overlayCard}`}>
+                        <img
+                          srcSet={`
                     ${process.env.PUBLIC_URL}/images/mob/public-slide/green_pouf@2_mob_270.png 540w,
                     ${process.env.PUBLIC_URL}/images/mob/public-slide/green_pouf_mob_270.png 270w
                     `}
-                      sizes="(min-width: 375px) 270px, 100vw"
-                      src={
-                        process.env.PUBLIC_URL +
-                        "/images/mob/public-slide/green_pouf_mob_270.png"
-                      }
-                      alt="Green puof"
-                    />
+                          sizes="(min-width: 375px) 270px, 100vw"
+                          src={
+                            process.env.PUBLIC_URL +
+                            "/images/mob/public-slide/green_pouf_mob_270.png"
+                          }
+                          alt="Green puof"
+                        />
 
-                    <p className={css.overlayText}>
-                      Краткая информация. Цена <a href="/">link</a>
-                    </p>
-                  </div>
-                  <p className={css.lpName}>Green puof</p>
-                </article>
-              </li>
-              <li classNamclassName={`${css.swiperSlide} ${css.lpItem}`}>
-                <article className={css.lpCard}>
-                  <div className={`${css.lpImgBox} ${css.overlayCard}`}>
-                    <img
-                      srcSet={`
+                        <p className={css.overlayText}>
+                          Краткая информация. Цена <a href="/">link</a>
+                        </p>
+                      </div>
+                      <p className={css.lpName}>Green puof</p>
+                    </article>
+                  </li>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <li className={`${css.swiperSlide} ${css.lpItem}`}>
+                    <article className={css.lpCard}>
+                      <div className={`${css.lpImgBox} ${css.overlayCard}`}>
+                        <img
+                          srcSet={`
                     ${process.env.PUBLIC_URL}/images/mob/public-slide/terracotta_pouf@2_mob_270.png 540w,
                     ${process.env.PUBLIC_URL}/images/mob/public-slide/terracotta_pouf_mob_270.png 270w
                     `}
-                      sizes="(min-width: 375px) 270px, 100vw"
-                      src={
-                        process.env.PUBLIC_URL +
-                        "/images/mob/public-slide/terracotta_pouf_mob_270.png"
-                      }
-                      alt="Terracotta pouf"
-                    />
+                          sizes="(min-width: 375px) 270px, 100vw"
+                          src={
+                            process.env.PUBLIC_URL +
+                            "/images/mob/public-slide/terracotta_pouf_mob_270.png"
+                          }
+                          alt="Terracotta pouf"
+                        />
 
-                    <p className={css.overlayText}>
-                      Краткая информация. Цена <a href="/">link</a>
-                    </p>
-                  </div>
+                        <p className={css.overlayText}>
+                          Краткая информация. Цена <a href="/">link</a>
+                        </p>
+                      </div>
 
-                  <p className={css.lpName}>Terracotta pouf</p>
-                </article>
-              </li>
+                      <p className={css.lpName}>Terracotta pouf</p>
+                    </article>
+                  </li>
+                </SwiperSlide>
+              </Swiper>
             </ul>
             <div className={css.swiperScrollbar}></div>
           </div>
