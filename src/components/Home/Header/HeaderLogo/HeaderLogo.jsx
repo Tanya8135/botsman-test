3f5320a80642fd219a80ce92ee207b83c3c2a6c1
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-08-13 12:36:21 +0300

    delete unnecessary styles

--- src/components/Home/Header/HeaderLogo/HeaderLogo.jsx
+++ src/components/Home/Header/HeaderLogo/HeaderLogo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import logo from '../../../../img/logoBotsmanMin.svg'
+import logo from "../../../../img/logoBotsmanMin.svg";
 
 import css from "./HeaderLogo.module.css";
 
